305a487e285e34a0de8c05d0c57f8f2f2c3a3c7c
Jörg Prante <joergprante@gmail.com>
2013-07-25 13:22:55 +0200

    snapshot

--- search/elements/src/main/groovy/org/xbib/analyzer/output/dublincore.groovy
+++ search/elements/src/main/groovy/org/xbib/analyzer/output/dublincore.groovy
@@ -1,9 +1,10 @@
 package org.xbib.analyzer.output
 import org.xbib.analyzer.dublincore.DublinCoreContext
 import org.xbib.elements.DefaultElementOutput
+import org.xbib.rdf.xcontent.ContentBuilder
 
 public class DublinCoreOutput extends DefaultElementOutput<DublinCoreContext> {
-    public void output(DublinCoreContext context) {
+    public void output(DublinCoreContext context, ContentBuilder builder) {
          println 'scripted output, got resource ' + context.resource()
          return
     }
