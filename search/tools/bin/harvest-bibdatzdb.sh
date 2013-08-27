0e6071d5b165ab2a55abc5d27791eb19fae1a295
Jörg Prante <joergprante@gmail.com>
2013-08-27 15:54:52 +0200

    snapshot

--- search/tools/bin/harvest-bibdatzdb.sh
+++ search/tools/bin/harvest-bibdatzdb.sh
@@ -3,7 +3,7 @@
 for d in `seq 2000 2013`; do
   e=`expr ${d} + 1`
   java \
-    -cp bin:lib/xbib-tools-1.0-SNAPSHOT-content.jar \
+    -cp bin:lib/tools-1.0-SNAPSHOT-content.jar \
     org.xbib.tools.harvest.BibdatZDB \
     --server "http://services.dnb.de/oai/repository" \
     --prefix "PicaPlus-xml" \
